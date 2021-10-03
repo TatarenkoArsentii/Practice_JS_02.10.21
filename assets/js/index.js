9f34e3e9033ab96d0a257f40c49cec733d24abee
Arsentii Tatarenko <tatarenkoarsen@gmail.com>
2021-10-03 11:29:42 +0300

    Slider 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,38 @@
-"use strict";
-const slideEl = document.querySelectorAll(".slide");
-let activeItemEl = document.querySelector(".active");
+const colorSlider = document.querySelector(".slider-color-boxes");
+const slidesCount = colorSlider.querySelectorAll("div").length;
+// 2 slider
+const imgSlider = document.querySelector(".slider-images");
+// control buttons
+const upButton = document.querySelector(".btn-up");
+const downButton = document.querySelector(".btn-down");
+// container
+const container = document.querySelector(".container");
+// current slide index
+let index = 0;
 
-slideEl.forEach((el) => {
-  el.addEventListener("click", activeSlide);
+// imgSlider.style.top = `-${(slidesCount - 1) * 100}vh`;
+// colorSlider.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => {
+  moveToUpDown("up");
 });
 
-function activeSlide() {
-  if (activeItemEl) activeItemEl.classList.remove("active");
-  activeItemEl = this;
-  this.classList.add("active");
+downButton.addEventListener("click", () => {
+  moveToUpDown("down");
+});
+
+function moveToUpDown(dirMove) {
+  if (dirMove == "up") {
+    index = (index + 1) % slidesCount;
+  } else if (dirMove == "down") {
+    index = (index - 1 + slidesCount) % slidesCount;
+  } else {
+    console.error("Unknown parameter");
+  }
+  console.log("index = ", index);
+  const offsetHeight = container.clientHeight;
+  colorSlider.style.transform = `translateY(-${index * offsetHeight}px)`;
+  imgSlider.style.transform = `translateY(-${index * offsetHeight}px)`;
+  console.log(colorSlider.style.transform);
+  console.log(imgSlider.style.transform);
 }
